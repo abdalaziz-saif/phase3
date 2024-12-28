610d943cff5e9bda1b61c3de2d59a36251759d42
fatimamoh2 <146951443+fatimamoh2@users.noreply.github.com>
2024-12-28 22:09:15 +0200

    Update models.py

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/')
+    quantity = models.PositiveIntegerField(default=0)
+
+    def _str_(self):
+        return self.name
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def get_total_price(self):
+        return self.quantity * self.product.price
