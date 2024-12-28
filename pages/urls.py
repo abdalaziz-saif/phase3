01447de09e0933d7bc4e5a56e9fcc687fe64fc79
fatimamoh2 <146951443+fatimamoh2@users.noreply.github.com>
2024-12-28 22:12:25 +0200

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,26 @@
 from django.urls import path
-from . import views
+from . import views(
+login,
+product_list,
+add_to_cart,
+cart_details,
+remove_from_cart,
+
+
+)
 
 urlpatterns = [
-    path('login/',views.login, name='login'),
+     path('login/',login, name='login'),
+    path('', product_list, name='product_list'),
+    path('add-to-cart/', add_to_cart, name='add_to_cart'),
+    path('cart/', cart_details , name='cart_details'),
+    path('remove-from-cart/', remove_from_cart, name='remove_from_cart'),
+]
+
+
+   
        
 
 
 
- ]
+
